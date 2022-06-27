30574d8678d8f1d63cabfd71d1e0fadea04c996b
niloy <n.niloy06091996@gmail.com>
2022-06-27 13:43:52 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pickaboo-frontend-clone
+ it is a clone of the pickaboo website's frontend design using Angular
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { ProductComponent } from './components/product/product.component';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pickaboo-clone';
+
+  ngOnInit(){
+    // let fix = new ProductComponent();
+    // let active = fix.bool;
+  }
+}
